8c027b83873fac3c1901b9b65b8a8be175973653
vagrant <vagrant@vagrant.vm>
2017-11-24 20:29:20 +0000

    methods for main features

--- Restaurant-Menu-App.py
+++ Restaurant-Menu-App.py
@@ -1,12 +1,50 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/restaurants')
+def show_restaurants():
+    return render_template('index.html')
+
+
+@app.route('/restaurant/new')
+def create_restaurant():
+    pass
+
+
+@app.route('/restaurant/<int:restaurant_id>/edit')
+def edit_restaurant(restaurant_id):
+    pass
+
+
+@app.route('/restaurant/<int:restaurant_id>/delete')
+def delete_restaurant(restaurant_id):
+    pass
+
+
+@app.route('/restaurant/<int:restaurant_id>')
+@app.route('/restaurant/<int:restaurant_id>/menu')
+def show_menu(restaurant_id):
+    pass
+
+
+@app.route('/restaurant/<int:restaurant_id>/menu/new')
+def create_menu(restaurant_id):
+    pass
+
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit')
+def edit_menu(restaurant_id, menu_id):
+    pass
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete')
+def delete_menu(restaurant_id, menu_id):
+    pass
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
